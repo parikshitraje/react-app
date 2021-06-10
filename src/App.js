5efbc5743b689b68f44e6adb42f08b715fd073b5
Parikshit J <location.downtown@gmail.com>
2021-06-11 01:47:36 +0530

    First changes in Home page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to React World !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Parikshit Jadhav
+        </p>
       </header>
     </div>
   );
